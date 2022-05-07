3aee1df34a64600ed2ad9ffc88aefde0c0ebda4d
Andres Arana <3av.arana@gmail.com>
2022-05-06 20:19:43 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Victor Arana on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
